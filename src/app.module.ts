f4fde923b5cc53f2c1db13fc885b649fceda805d
ini-agung <cisea.dev@gmail.com>
2023-06-09 16:05:49 +0700

    fix sql module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,32 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import configuration from './config/configuration';
+
 @Module({
-  imports: [AuthModule, UsersModule, UsersModule],
+  imports: [
+    ConfigModule.forRoot(
+      {
+        isGlobal: true,
+        load: [configuration],
+      }
+    ),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DATABASE_HOST,
+      port: 3306,
+      username: process.env.DATABASE_USERNAME,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME,
+      entities: [],
+      synchronize: true,
+    }),
+    AuthModule,
+    UsersModule,
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
-
-
-
 export class AppModule { }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { log } from 'console';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const port = 3000;
+  log('Listen on port', port)
+  await app.listen(port);
 }
 bootstrap();
